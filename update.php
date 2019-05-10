9ac0d2453d773715a2bf13a73a5cf42372fea77a
sreeraj <sreerajavk123@gmail.com>
2019-05-10 22:20:40 +0530

    updatefood modified

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 
     $content = $_GET['content']; 
 
-    $myfile = fopen("dishes2.xml", "w") or die("Unable to open file!");
+    $myfile = fopen("dishes.xml", "w") or die("Unable to open file!");
     fwrite($myfile, $content);
 
     fclose($myfile);
--- updatefood.php
+++ updatefood.php
@@ -35,18 +35,14 @@
                             </tbody>
                         </table>
                         
+                        <button type="button" id="bt" style="margin-bottom:2em;" class="btn btn-success">&nbsp;Update&nbsp;&nbsp;</button>
+                        
                     </div>
                 </div>
             </div>
-            
-            
-            
-            
-            
-
     <script>
 
-            // $('#bt').click(function () {
+            
 
                 var xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function () {
@@ -60,12 +56,17 @@
                 xhttp.send();
                 function parseXml(response) {
                         var xmlDoc = response.responseXML;
+                        var count=0;
+                        var xmlString = "<hoteldishes></hoteldishes>";
+                        var parser = new DOMParser();
+                        var root = parser.parseFromString(xmlString, "text/xml"); 
+                        var dishes = root.createElement("hotelinfo");
                     
                         for(let name of xmlDoc.getElementsByTagName("hotel")){
 
                             //getting hotelname from get request
                             var hotelname = "<?php echo $_GET['name']?>";
-                            
+        
                             //accessing the table body element
                             var table_body = $("#table_body");
                             // checking the hotelname
@@ -77,10 +78,18 @@
                                     //dynamically adding rows
                                     var tr = $('<tr></tr>');
                                     var td1 = $('<td>'+dishes.childNodes[0].nodeValue+'</td>');
-                                    var td2 = $('<td><div class="checkbox"><label><input type="checkbox" value=""></label></div></td>');
+                                    var td2 = $('<td><div class="checkbox"><label><input type="checkbox" id="id'+count+'"value=""></label></div></td>');
                                     tr.append(td1);
                                     tr.append(td2);
                                     table_body.append(tr);
+                                    count+=1;
+
+
+                                    //creating the xml file
+                                    // add an edition element
+                                    var newEle = root.createElement("dishes");
+
+                                    var newText = xmlDoc.createTextNode("first");
                                     // var td =  $('<tr>'+name.getElementsByTagName("availabiliy"))
 
                                 }
@@ -101,10 +110,67 @@
                             // }
                         }
 
+                
+
+                $('#bt').click(function () {
+                    // alert("Sree")
+
+                    for(let name of xmlDoc.getElementsByTagName("hotel")){
+
+                        var count=0;
+                        var hotel = root.createElement("hotel");
+                        // root.appendChild(hotel)
+                        // root.appendChild(hotel);
+                        
+                        hotel.setAttribute("id" , name.getAttribute('id'))
+                            for(let dishes of name.getElementsByTagName("name")){
+
+                                let item = root.createElement("item");
+                                let hotel_name = root.createElement("name");
+                                let avail = root.createElement("availability");
+                                hotel_name.appendChild(root.createTextNode(dishes.childNodes[0].nodeValue));
+
+                                if(name.getAttribute('id')==hotelname){
+
+                                    // avail.createTextNode()
+                                    if(document.getElementById('id'+count).checked)
+                                        avail.appendChild(root.createTextNode("Yes"));
+                                    else    
+                                        avail.appendChild(root.createTextNode("No"));                               
+                                }
+                                else{
+                                    console.log(name.getElementsByTagName("available")[0].childNodes[0].nodeValue);
+                                    let available = name.getElementsByTagName("available")[0].childNodes[0].nodeValue;
+                                    avail.appendChild(root.createTextNode(available));
+                                }
+                                item.appendChild(hotel_name);
+                                item.appendChild(avail);
+                                hotel.appendChild(item);
+                                count+=1;
+                            
+                        }
+
+                        dishes.appendChild(hotel);
+                        
+                    }
+                    console.log(dishes);
+                    var xmlText = new XMLSerializer().serializeToString(dishes);
+
+                    var xhttp2 = new XMLHttpRequest();
+                    xhttp2.onreadystatechange = function () {
+                    if(xhttp2.readyState == 4 && xhttp2.status == 200){
+
+                        alert("Successfully updated");
+
+                        }
+                    }
+                    xhttp2.open("GET", "update.php?content="+xmlText , true);
+                    xhttp2.send();
+  
+                    
+                    
+                });
                 }
-
-
-            // });
 
         </script>
 
